f2205eaa9f751d72da2ef1a9dd19da405b8b9051
Muchiri-cmd <davismuchiri21@gmail.com>
2025-07-28 11:44:47 +0300

    Fix Bug

--- client/src/utils/axiosInstance.ts
+++ client/src/utils/axiosInstance.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   // baseURL: "http://localhost:3000/api",
   // baseURL: "http://192.168.1.122:3000/api",
-  baseURL:"https://notely-backend-3ldy.onrender.com/",
+  baseURL:"https://notely-backend-3ldy.onrender.com/api",
   withCredentials: true,
 });
